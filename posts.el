c934b3edbd9a82934b210da53497373da7039946
0x736561 <dfern3321@gmail.com>
2024-02-18 12:37:11 -0300

    Múltiplas paletas de cores

--- posts.el
+++ posts.el
@@ -3,7 +3,8 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style t ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"../src/styles.css \" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"../src/styles.css \" />"
+      org-html-head-extra "<link rel=\"stylesheet\" href=\"../src/colors.css \" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
